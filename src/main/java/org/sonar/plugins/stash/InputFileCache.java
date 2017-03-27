4fd5bf29eb2f36bfeb4a948a52bd3ae4bfc896da
Oleg Marchuk <oleg_m@xwiserd.com>
2017-03-27 18:26:10 +0300

    Code style

--- src/main/java/org/sonar/plugins/stash/InputFileCache.java
+++ src/main/java/org/sonar/plugins/stash/InputFileCache.java
@@ -1,17 +1,32 @@
 package org.sonar.plugins.stash;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.CheckForNull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.api.BatchComponent;
 import org.sonar.api.batch.InstantiationStrategy;
 import org.sonar.api.batch.fs.InputFile;
 
-import javax.annotation.CheckForNull;
-import java.util.HashMap;
-import java.util.Map;
-
 @InstantiationStrategy(InstantiationStrategy.PER_BATCH)
 public class InputFileCache implements BatchComponent {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(InputFileCache.class);
+	
   private final Map<String, InputFile> inputFileByKey = new HashMap<>();
+  
+  public InputFileCache() {
+	  LOGGER.debug("New InputFileCache created");
+  }
+  
+  // For debug purpose
+  public Map<String, InputFile> getInputFileByKeyMap() {
+	  return Collections.unmodifiableMap(inputFileByKey);
+  }
   
   public void putInputFile(String componentKey, InputFile inputFile) {
     inputFileByKey.put(componentKey, inputFile);
