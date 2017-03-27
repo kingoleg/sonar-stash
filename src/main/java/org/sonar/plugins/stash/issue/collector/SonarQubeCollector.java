4f637e008ba832ba2066d3c7ebf5431515bd2950
Oleg Marchuk <oleg_m@xwiserd.com>
2017-03-27 19:13:00 +0300

    Code style

--- src/main/java/org/sonar/plugins/stash/issue/collector/SonarQubeCollector.java
+++ src/main/java/org/sonar/plugins/stash/issue/collector/SonarQubeCollector.java
@@ -12,36 +12,35 @@ import org.sonar.plugins.stash.IssuePathResolver;
 
 public final class SonarQubeCollector {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(SonarQubeCollector.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SonarQubeCollector.class);
 
-  private SonarQubeCollector() {}
+    private SonarQubeCollector() {
+    }
 
-  /**
-   * Create issue report according to issue list generated during SonarQube
-   * analysis.
-   */
-  public static List<Issue> extractIssueReport(ProjectIssues projectIssues, IssuePathResolver issuePathResolver) {
-    return StreamSupport.stream(
-                         projectIssues.issues().spliterator(), false)
-                        .filter(issue -> shouldIncludeIssue(issue, issuePathResolver))
-                        .collect(Collectors.toList());
-  }
+    /**
+     * Create issue report according to issue list generated during SonarQube
+     * analysis.
+     */
+    public static List<Issue> extractIssueReport(ProjectIssues projectIssues, IssuePathResolver issuePathResolver) {
+        return StreamSupport.stream(projectIssues.issues().spliterator(), false)
+                .filter(issue -> shouldIncludeIssue(issue, issuePathResolver)).collect(Collectors.toList());
+    }
 
-  // TODO exclude issue not related to diff
+    // TODO exclude issue not related to diff
     private static boolean shouldIncludeIssue(Issue issue, IssuePathResolver issuePathResolver) {
-    if (!issue.isNew()){
-      LOGGER.debug("Issue {} is not a new issue and so, not added to the report", issue.key());
-      return false;
-    }
+        if (!issue.isNew()) {
+            LOGGER.debug("Issue {} is not a new issue and so, not added to the report", issue.key());
+            return false;
+        }
 
-    String path = issuePathResolver.getIssuePath(issue);
-    if (path == null) {
-			LOGGER.debug(
-					"Issue {} is not linked to a file, not added to the report, issue.componentKey = {}, issue.actionPlanKey = {}, issue.key = {}, issue.ruleKey = {}, issue.message = {}, issue.line = {}, issue.resolution = {}, issue.attributes = {}",
-					issue, issue.componentKey(), issue.actionPlanKey(), issue.key(), issue.ruleKey(), issue.message(),
-					issue.line(), issue.resolution(), issue.attributes());
-      return false;
+        String path = issuePathResolver.getIssuePath(issue);
+        if (path == null) {
+            LOGGER.debug(
+                    "Issue {} is not linked to a file, not added to the report, issue.componentKey = {}, issue.actionPlanKey = {}, issue.key = {}, issue.ruleKey = {}, issue.message = {}, issue.line = {}, issue.resolution = {}, issue.attributes = {}",
+                    issue, issue.componentKey(), issue.actionPlanKey(), issue.key(), issue.ruleKey(), issue.message(),
+                    issue.line(), issue.resolution(), issue.attributes());
+            return false;
+        }
+        return true;
     }
-    return true;
-  }
 }
